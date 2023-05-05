63c09e2b0f050452d24ca000a5fa01faa2116f13
Bipul chhetri <bipulchhetri80@gmail.com>
2023-05-05 21:19:52 +0530

    React first project
    React first project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import Header from './components/Header';
 import Home from './components/Home';
-import About from './components/About';
+
 
 import './Style/Header.scss'
 import './Style/App.scss'
